4cc50c17b443f88bbf9ee48e7f734b381785af3f
Mohammed Syam <m7mdsyam_cs@yahoo.com>
2018-03-25 19:30:41 +0200

    Update UpdateHostName.sh

--- UpdateHostName.sh
+++ UpdateHostName.sh
@@ -1,40 +1,43 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Path to your hosts file
-hostsFile="/etc/hosts"
+# copy from https://gist.github.com/irazasyed/a7b0a079e7727a4315b9
 
-# Default IP address for host
-ip="127.0.0.1"
+# PATH TO YOUR HOSTS FILE
+ETC_HOSTS=/etc/hosts
+
+# DEFAULT IP FOR HOSTNAME
+IP="127.0.0.1"
 
 # Hostname to add/remove.
-hostname="Sandbox2-hdp.hortonworks.com"
+HOSTNAME="sandbox2-hdp.hortonworks.com"
 
-yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 111; }
-try() { "$@" || die "cannot $*"; }
-
-remove() {
-    if [ -n "$(grep -P "[[:space:]]$hostname" /etc/hosts)" ]; then
-        echo "$hostname found in $hostsFile. Removing now...";
-        try sudo sed -ie "/[[:space:]]$hostname/d" "$hostsFile";
+removehost() {
+    echo "removing host";
+    if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+    then
+        echo "$HOSTNAME Found in your $ETC_HOSTS, Removing now...";
+        sudo sed -i".bak" "/$HOSTNAME/d" $ETC_HOSTS
     else
-        yell "$hostname was not found in $hostsFile";
+        echo "$HOSTNAME was not found in your $ETC_HOSTS";
     fi
 }
 
-add() {
-    if [ -n "$(grep -P "[[:space:]]$hostname" /etc/hosts)" ]; then
-        yell "$hostname, already exists: $(grep $hostname $hostsFile)";
-    else
-        echo "Adding $hostname to $hostsFile...";
-        try printf "%s\t%s\n" "$ip" "$hostname" | sudo tee -a "$hostsFile" > /dev/null;
-
-        if [ -n "$(grep $hostname /etc/hosts)" ]; then
-            echo "$hostname was added succesfully:";
-            echo "$(grep $hostname /etc/hosts)";
+addhost() {
+    echo "adding host";
+    HOSTS_LINE="$IP\t$HOSTNAME"
+    if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+        then
+            echo "$HOSTNAME already exists : $(grep $HOSTNAME $ETC_HOSTS)"
         else
-            die "Failed to add $hostname";
-        fi
+            echo "Adding $HOSTNAME to your $ETC_HOSTS";
+            sudo -- sh -c -e "echo '$HOSTS_LINE' >> /etc/hosts";
+
+            if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+                then
+                    echo "$HOSTNAME was added succesfully \n $(grep $HOSTNAME /etc/hosts)";
+                else
+                    echo "Failed to Add $HOSTNAME, Try again!";
+            fi
     fi
 }
 
